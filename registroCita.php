8f516aa64174a4e4c58cf33046b8cd3a743649bc
Jesus Armando <armando_saenz@live.com.mx>
2021-06-18 14:45:45 -0500

    Registrar cita

--- registroCita.php
+++ registroCita.php
@@ -4,11 +4,7 @@
     session_start();
 
     if(!isset($_SESSION['rol'])){
-        header('location: login.php');
-    }else{
-        if($_SESSION['rol'] != 1){
-            header('location: login.php');
-        }
+        header('location: login.php)');
     }
 
     if(isset($_GET['cerrar_sesion'])){
@@ -25,6 +21,48 @@
 
 	$data = $querylista->fetchAll();
 
+    if(isset($_POST['username']) && isset($_POST['servicio']) && isset($_POST['fecha']) && isset($_POST['hora'])){
+        $username = $_POST['username'];
+        $servicio = $_POST['servicio'];
+        $fecha = $_POST['fecha'];
+        $hora = $_POST['hora'];
+        $cita = $fecha." ".$hora;
+        //echo"<script>alert('".$cita."');</script>";
+    
+        $query = $db->connect()->prepare("CALL SP_agregarCita(:username,:servicio,:cita)");
+	    $query->execute([':username' => $username, ':servicio' => $servicio, ':cita' => $cita]);
+
+	    $row = $query->fetch(PDO::FETCH_NUM);
+	
+	    if($row == true){
+		    $rol = $row[0];
+		    if($rol==0){
+		    	echo"<script>alert('La fecha no ha sido ingresada correctamente!'); </script>";
+		    }else if($rol== 1){
+			    echo"<script>alert('Se ha registrado su cita correctamente!'); </script>";
+		    }
+		
+	    }else{
+		    // no existe el usuario
+		    echo "</script>Error</script>";
+	    }
+
+
+
+
+
+
+
+
+
+
+    }
+        
+    
+
+
+    
+
     
 ?>
 
@@ -34,11 +72,28 @@
 <head>
     <title >Proyecto Programacion 2
     </title>
-    <script src="/node_modules/jquery/dist/jquery.min.js"></script>
-    
     <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
-
     <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker3.min.css">
+    
+    <link rel="stylesheet" href="dist/bootstrap-clockpicker.min.css">
+    
+    <script type="text/javascript">
+        function ShowSelected()
+        {
+            /* Para obtener el valor */
+            var cod = document.getElementById("servicio").value;
+
+            //alert(cod);
+ 
+            /* Para obtener el texto */
+            var combo = document.getElementById("servicio");
+            var selected = combo.options[combo.selectedIndex].text;
+            //alert(selected);
+        }
+    </script>
+
+
 </head>
 
 <body>
@@ -84,18 +139,18 @@
                         <h4>Registro de Cita</h4>
                         <form method="POST" class="my-login-validation" novalidate="">
                             <div class="form-group">
-                                <label for="name"><p>Usuario(Tu cuenta)</p></label><br>
-                                <input id="name" type="text" class="form-control" name="name" required autofocus value="<?php echo $_SESSION['username'] ?>" readonly>
+                                <label for="username"><p>Usuario(Tu cuenta)</p></label><br>
+                                <input id="username" type="text" class="form-control" name="username" required autofocus value="<?php echo $_SESSION['username'] ?>" readonly>
                                 
                             </div>
                            <br>
 
                             <div class="input-group mb-3">
                                 <br>
-                                <label class="input-group-text" for="inputGroupSelect01">Servicios a elegir</label>
-                                <select class="form-select" id="inputGroupSelect01">
+                                <label class="input-group-text" for="servicio" >Servicios a elegir</label>
+                                <select class="form-select" id="servicio" onchange="ShowSelected();" name="servicio">
                                 <?php foreach ($data as $row):
-                                    echo '<option value="'.$row["idServicio"].'">'.$row["nombreServicio"].'</option>';
+                                    echo '<option value="'.$row["nombreServicio"].'">'.$row["nombreServicio"].'</option>';
                                     endforeach;
                                 ?>
                                 </select>
@@ -104,22 +159,40 @@
                             
                             
                             <div class="form-group">
-                                <label for="cuenta"><p>Fecha y Hora de la Cita</p></label><br>
-                                <input id="cuenta" type="text" class="form-control" name="cuenta" required>
+                                <label for="fecha"><p>Fecha y Hora de la Cita</p></label><br>
+                                
+                                
+                                <div class="input-group date datepicker" data-provide="datepicker" name="fecha">
+                                    <input id="fecha" type="text" class="form-control" required name="fecha">
+                                    <div class="input-group-addon">
+                                    <span class="form-control btn-secondary"><i class="fas fa-calendar-day"></i></span>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="input-group clockpicker" name="hora">
+                                    <input id="hora" type="text" class="form-control" value="09:30" name="hora">
+                                    <span class="input-group-addon">
+                                    <span class="form-control btn-secondary"><i class="far fa-clock"></i></span>
+                                </span>
+                                </div>
+
+
+
                                 
                             </div>
                             
                             <br>
 
 
+                            <br>
+
+
                             <div class="form-group m-0">
                                 <button type="submit" class="btn btn-primary btn-block">
-                                    Registrar
+                                    Registrar Cita
                                 </button>
                             </div>
-                            <div class="mt-4 text-center">
-                                Ya tienes una cuenta? <a href="login.php">Inicio de Sesi&#243;n</a>
-                            </div>
+                            
                         </form>
                     </div>
                 
@@ -131,23 +204,41 @@
 </div>
 
 
-
-
-
-
-
 </div>
 
 
+    
+    
 
-
-
-
-
+    <script src="/node_modules/jquery/dist/jquery.min.js"></script>
     <script src="/node_modules/@popperjs\core/dist/umd/popper.min.js"></script>
     <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="/js/main.js"></script>
+    
+    <script src="/js/bootstrap-datepicker.js"></script>
+    <script src="/js/bootstrap-datepicker.min.js"></script>
+    <script src="/locales/bootstrap-datepicker.es.min.js"></script>        
+    <script>
+        $('.datepicker').datepicker({
+        format: 'yyyy-mm-dd',
+        language:'es',
+        daysOfWeekDisabled: '0,6',
+        todayHighlight: true,
+        weekStart: '0',
+        startDate: new Date()
+        });
+    </script>
+    
+    <script src="/dist/bootstrap-clockpicker.min.js"></script>
+
+    <script>
+        $('.clockpicker').clockpicker({
+            donetext: "Elegir hora",
+            placement: 'bottom',
+            autoclose: true,
+        });
+    </script>
+
 </body>
 
 </html>
-
