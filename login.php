51b608a819ba95c17a31e0d41c78c37f593cb6af
Jesus Armando <armando_saenz@live.com.mx>
2021-06-18 21:41:39 -0500

    Navbarbien

--- login.php
+++ login.php
@@ -78,8 +78,8 @@ if(isset($_POST['username']) && isset($_POST['password'])){
 <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
     <a class="navbar-brand" href="/index.php"><p>Dentista</p></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler  navbar-light bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon "></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
